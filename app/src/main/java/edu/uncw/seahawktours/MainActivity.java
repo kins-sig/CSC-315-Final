fedc408343719279b97b699f9fc76d610a94f077
Joren McSorley <joren.mcsorley@gmail.com>
2018-10-10 15:42:53 -0400

    small update. started an activity for the buildings view.

--- app/src/main/java/edu/uncw/seahawktours/MainActivity.java
+++ app/src/main/java/edu/uncw/seahawktours/MainActivity.java
@@ -2,6 +2,8 @@ package edu.uncw.seahawktours;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onClickFindBuilding(View view){
+        //Get a reference to the Spinner
+        Spinner buildings = findViewById(R.id.buildings);
+        //Get the selected item in the Spinner
+        String building = String.valueOf(buildings.getSelectedItem());
     }
 }
