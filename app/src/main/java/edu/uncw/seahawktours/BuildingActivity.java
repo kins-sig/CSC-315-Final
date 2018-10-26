103e232e5f088807712f0ae65d99dfe44bf4b290
Joren McSorley <joren.mcsorley@gmail.com>
2018-10-26 10:46:13 -0400

    Added url link to the UNCW building page for each building.

--- app/src/main/java/edu/uncw/seahawktours/BuildingActivity.java
+++ app/src/main/java/edu/uncw/seahawktours/BuildingActivity.java
@@ -1,7 +1,10 @@
 package edu.uncw.seahawktours;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,6 +18,8 @@ public class BuildingActivity extends Activity {
         ImageView buildingImage = findViewById(R.id.building_image);
         TextView buildingCaption = findViewById(R.id.building_caption);
         TextView buildingDescription = findViewById(R.id.building_description);
+        TextView link = findViewById(R.id.building_url);
+        String url = null;
         Bundle bundle = getIntent().getExtras();
         assert bundle != null;
         String message = bundle.getString("building");
@@ -24,30 +29,47 @@ public class BuildingActivity extends Activity {
             buildingImage.setImageResource(R.drawable.cis_building);
             buildingCaption.setText(R.string.cis_caption);
             buildingDescription.setText(R.string.cis_description);
+            url = getString(R.string.cis_url);
         }
         else if (message.equals(getString(R.string.trask_building))){
             buildingName.setText(R.string.trask_building);
             buildingImage.setImageResource(R.drawable.trask_building);
             buildingCaption.setText(R.string.trask_caption);
             buildingDescription.setText(R.string.trask_description);
+            url = getString(R.string.trask_url);
         }
         else if (message.equals(getString(R.string.bear_building))){
             buildingName.setText(R.string.bear_building);
             buildingImage.setImageResource(R.drawable.bear_building);
             buildingCaption.setText(R.string.bear_caption);
             buildingDescription.setText(R.string.bear_description);
+            url = getString(R.string.bear_url);
         }
         else if (message.equals(getString(R.string.alderman_building))) {
             buildingName.setText(R.string.alderman_building);
             buildingImage.setImageResource(R.drawable.alderman_building);
             buildingCaption.setText(R.string.alderman_caption);
             buildingDescription.setText(R.string.alderman_description);
+            url = getString(R.string.alderman_url);
         }
         else if (message.equals(getString(R.string.belk_building))) {
             buildingName.setText(R.string.belk_building);
             buildingImage.setImageResource(R.drawable.belk_building);
             buildingCaption.setText(R.string.belk_caption);
             buildingDescription.setText(R.string.belk_description);
+            url = getString(R.string.belk_url);
         }
+
+        final String finalUrl = url;
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse(finalUrl));
+                startActivity(intent);
+            }
+        });
     }
 }
