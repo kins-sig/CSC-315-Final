ea2e92dfd1f21cbf77b3dee6b10ced63089bb5f4
Joren McSorley <joren.mcsorley@gmail.com>
2018-10-30 13:46:21 -0400

    fixed toolbar

--- app/src/main/java/edu/uncw/seahawktours/BuildingActivity.java
+++ app/src/main/java/edu/uncw/seahawktours/BuildingActivity.java
@@ -3,7 +3,6 @@ package edu.uncw.seahawktours;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.SpannableString;
--- app/src/main/java/edu/uncw/seahawktours/MainActivity.java
+++ app/src/main/java/edu/uncw/seahawktours/MainActivity.java
@@ -1,19 +1,15 @@
 package edu.uncw.seahawktours;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
+import android.support.v7.widget.Toolbar;
 import android.widget.Spinner;
 
-import java.util.ArrayList;
-
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +20,8 @@ public class MainActivity extends Activity {
         ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, spinnerArray);
         spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         buildings.setAdapter(spinnerArrayAdapter);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     public void onClickDisplayBuilding(View view){
