f3f801dd24a22a4ca0f8337d2bb350a45c8d15e3
Joren McSorley <joren.mcsorley@gmail.com>
2018-11-01 11:01:17 -0400

    updates

--- app/src/main/java/edu/uncw/seahawktours/AboutActivity.java
+++ app/src/main/java/edu/uncw/seahawktours/AboutActivity.java
@@ -1,11 +1,20 @@
 package edu.uncw.seahawktours;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.TextPaint;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -15,6 +24,28 @@ public class AboutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_about);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        TextView link = findViewById(R.id.uncw_buildings_website);
+        SpannableString ss = new SpannableString(getString(R.string.learn_more_here));
+        ClickableSpan clickableSpan = new ClickableSpan() {
+            @Override
+            public void onClick(@NonNull View textView) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse(getString(R.string.uncw_buildings_website)));
+                startActivity(intent);
+            }
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setUnderlineText(false);
+                ds.setColor(getResources().getColor(R.color.uncw_yellow));
+            }
+        };
+        ss.setSpan(clickableSpan, 21, ss.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        link.setText(ss);
+        link.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     @Override
