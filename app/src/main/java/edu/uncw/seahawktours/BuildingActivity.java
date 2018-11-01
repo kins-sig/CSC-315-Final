008c6b593c9cba8d75b7a723d80cfd08772eed20
Joren McSorley <36486302+jorenmcsorley@users.noreply.github.com>
2018-11-01 17:20:52 -0400

    Share button displays the correct building url for the select building. If isn't clickable in the messanger. The url declarations in BuildingActivity aren't pretty but they work. The urls get used in multiple places so they are now in the outer scope.

--- app/src/main/java/edu/uncw/seahawktours/BuildingActivity.java
+++ app/src/main/java/edu/uncw/seahawktours/BuildingActivity.java
@@ -21,7 +21,12 @@ import android.support.v7.widget.ShareActionProvider;
 import android.support.v4.view.MenuItemCompat;
 
 public class BuildingActivity extends AppCompatActivity {
+    private String url;
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
+    final String finalUrl = url;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,7 +41,6 @@ public class BuildingActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         if (actionBar == null) throw new AssertionError();
         actionBar.setDisplayHomeAsUpEnabled(true);
-        String url = null;
         Bundle bundle = getIntent().getExtras();
         assert bundle != null;
         String message = bundle.getString("building");
@@ -46,38 +50,38 @@ public class BuildingActivity extends AppCompatActivity {
             buildingImage.setImageResource(R.drawable.cis_building);
             buildingCaption.setText(R.string.cis_caption);
             buildingDescription.setText(R.string.cis_description);
-            url = getString(R.string.cis_url);
+            setUrl(getString(R.string.cis_url));
+
         }
         else if (message.equals(getString(R.string.trask_building))){
             buildingName.setText(R.string.trask_building);
             buildingImage.setImageResource(R.drawable.trask_building);
             buildingCaption.setText(R.string.trask_caption);
             buildingDescription.setText(R.string.trask_description);
-            url = getString(R.string.trask_url);
+            setUrl(getString(R.string.trask_url));
         }
         else if (message.equals(getString(R.string.bear_building))){
             buildingName.setText(R.string.bear_building);
             buildingImage.setImageResource(R.drawable.bear_building);
             buildingCaption.setText(R.string.bear_caption);
             buildingDescription.setText(R.string.bear_description);
-            url = getString(R.string.bear_url);
+            setUrl(getString(R.string.bear_url));
         }
         else if (message.equals(getString(R.string.alderman_building))) {
             buildingName.setText(R.string.alderman_building);
             buildingImage.setImageResource(R.drawable.alderman_building);
             buildingCaption.setText(R.string.alderman_caption);
             buildingDescription.setText(R.string.alderman_description);
-            url = getString(R.string.alderman_url);
+            setUrl(getString(R.string.alderman_url));
         }
         else if (message.equals(getString(R.string.belk_building))) {
             buildingName.setText(R.string.belk_building);
             buildingImage.setImageResource(R.drawable.belk_building);
             buildingCaption.setText(R.string.belk_caption);
             buildingDescription.setText(R.string.belk_description);
-            url = getString(R.string.belk_url);
+            setUrl(getString(R.string.alderman_url));
         }
 
-        final String finalUrl = url;
         SpannableString ss = new SpannableString(getString(R.string.learn_more_here));
         ClickableSpan clickableSpan = new ClickableSpan() {
             @Override
@@ -101,14 +105,13 @@ public class BuildingActivity extends AppCompatActivity {
     }
 
     private ShareActionProvider shareActionProvider;
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         TextView buildingName = findViewById(R.id.building_name);
         getMenuInflater().inflate(R.menu.toolbar_menu_share, menu);
         MenuItem menuItem = menu.findItem(R.id.menu_share);
         shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
-        setShareActionIntent(getText(R.string.check_out_building) + buildingName.getText().toString()+ "\n\n" + "building url here");
+        setShareActionIntent(getText(R.string.check_out_building) + buildingName.getText().toString()+ "\n\n" + url);
         return true;
     }
 
